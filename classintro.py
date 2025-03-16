a64f4431f208c1b951ffe5d2edc43847d2e84dd4
feyza-koruk <feyza.koruk@boun.edu.tr>
2025-03-16 17:19:54 +0300

    made it more object oriented

--- classintro.py
+++ classintro.py
@@ -1,30 +1,31 @@
-i = 5
-def f():
-    j = 10
-    print (j)
 
-f()
 class Apple:  # class, concept
     type = "fruit"
     def __init__(self, loc = "tree", clr = "red"):
-        self._location = loc
-        self._color = clr
+        self.__location = loc
+        self.__color = clr
 
-    def _setlocation(self, l):
-        self._location = l
+    def get_location(self):
+        return self.__location
+    
+    def get_color(self): 
+        return self.__color
+
+    def _set_location(self, loc):
+        self.__location = loc
 
     def drop(self):
-        if self._location == "tree":
-            self._setlocation("ground")
+        if self.__location == "tree":
+            self._set_location("ground")
             print ("I dropped")
         else:
             print ("I connot be dropped")
+
 
 x = Apple("tree", "red") # object, tangible, implicitely calls __init__
 y = Apple()
 z = Apple(clr="yellow")
 
-# z._color = "black" # access to member variable. should not be allowed.
 z.drop() # if I designate drop function as public then ok. otherwise should not be allowed.
 
 x.drop() # Actually calls Apple.drop(x)
